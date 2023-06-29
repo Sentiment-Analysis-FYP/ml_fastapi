40c087d3509a6ca7a1b0e3750cce84306caf57c5
Max Otuteye <maxotuteye12@gmail.com>
2023-06-29 20:26:56 +0000

    fixing issue with file upload

--- controller/receive.py
+++ controller/receive.py
@@ -5,9 +5,22 @@ from fastapi import UploadFile
 
 async def receive_scrape(scrape_id: str, file: UploadFile = UploadFile(...)):
     save_directory = "/text_data/incomplete"
-    file_path = path.join(save_directory, f"{scrape_id}.json")
+    file_path = path.join("text_data", "incomplete", f"{scrape_id}.json")
+    # file_path = path.join(save_directory, f"{scrape_id}.json")
 
-    with open(file_path, "wb") as buffer:
-        buffer.write(await file.read())
+    try:
+        contents = file.file.read()
+        with open(file.filename, 'wb') as f:
+            f.write(contents)
 
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+
+    finally:
+        file.file.close()
+
+    # with open(file_path, "wb") as buffer:
+    #     buffer.write(await file.read())
+    #
+    # await file.close()
     return {"filename": f"{scrape_id}.json", "scrape_id": scrape_id}
