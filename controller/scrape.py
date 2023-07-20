e561a3c1c9bc822ef05e34b4c72e99354387fc65
Max Otuteye <maxotuteye12@gmail.com>
2023-07-20 01:04:17 +0000

    scrape helper functions

--- controller/scrape.py
+++ controller/scrape.py
@@ -1,35 +1,44 @@
-import tweepy
+import os
+
+from tweepy import API, OAuthHandler, Cursor
+from dotenv import load_dotenv
 
 
 async def run_scrape(scrape_id: str, data: dict):
     return
 
 
-def get_tweets(keywords, start_date, end_date):
-    consumer_key = 'YOUR_CONSUMER_KEY'
-    consumer_secret = 'YOUR_CONSUMER_SECRET'
-    access_token = 'YOUR_ACCESS_TOKEN'
-    access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
+def load_api_keys():
+    load_dotenv('../.env')
+    consumer_key = os.getenv('CONSUMER_KEY')
+    consumer_secret = os.getenv('CONSUMER_SECRET')
+    access_token = os.getenv('ACCESS_TOKEN')
+    access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+    return consumer_key, consumer_secret, access_token, access_token_secret
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+def get_tweets(keywords, start_date, end_date):
+    consumer_key, consumer_secret, access_token, access_token_secret = load_api_keys()
+
+    auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
-    api = tweepy.API(auth, wait_on_rate_limit=True)
+    api = API(auth, wait_on_rate_limit=True)
 
     try:
         # Format the query with multiple keywords joined by OR
         query = ' OR '.join(keywords)
 
         # Fetch tweets based on the query and date range
-        tweets = tweepy.Cursor(api.search, q=query, lang='en', tweet_mode='extended', since=start_date,
-                               until=end_date).items()
+        tweets = Cursor(api.search_tweets, q=query, lang='en', tweet_mode='extended', since=start_date,
+                        until=end_date).items()
 
         result = []
         for tweet in tweets:
-            result.append(tweet._json)
+            result.append(tweet.full_text)
 
         return result
 
-    except tweepy.TweepError as e:
+    except Exception as e:
         print("Error occurred during the search:", e)
         return []
